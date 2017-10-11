b4f525e8f216143d8dd40eb052af478a45d4ac7e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-11 19:48:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,9 @@ def position_taken?(board, index)
     false
   elsif(board[index]==" ")
     false
-elsif(board[index]==nil)
+ elsif(board[index]==nil)
   false
-  end
-  
+ elsif(board[index]="X" || board[index]=="O")
+  true
+end
 end
